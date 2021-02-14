53068135690fc7723ec32e730e7a9e5ba23220d2
thaohvm <43254779+thaohvm@users.noreply.github.com>
2021-02-13 23:45:00 -0800

    WIP profile

---
+++ network/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    follower = models.ManyToManyField(
+        "User", blank=True, related_name="user_follower")
+    following = models.ManyToManyField(
+        "User", blank=True, related_name="user_following")
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=256, blank=True)
+    time = models.DateTimeField(auto_now=True)
+    liked = models.ManyToManyField(User, blank=True, related_name="user_liked")
+
+    def __str__(self):
+        return f"{self.user}"
